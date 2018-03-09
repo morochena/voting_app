12863fffd68d005b4d203a4761c7ac7259a8147f
ajohe <arthur.johe@gmail.com>
2018-03-09 01:11:07 -0500

    make named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/help'
+  get '/home', to: 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'static_pages#home'
