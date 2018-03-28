32a72bda4a43e140b3830f76bfa1002b83ab77f2
ajohe <arthur.johe@gmail.com>
2018-03-28 04:04:31 -0400

    finish signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params) #not final implementation
+    if @user.save
+      redirect_to @user
+      flash[:success] =  "Welcome to This or That"
+    else
+      render "new"
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
 end
