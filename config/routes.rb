ff50ae3f8b7db0399e886d1b934c6375d48e6d0d
ajohe <arthur.johe@gmail.com>
2018-03-29 19:58:58 -0400

    create invalid user login message

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get '/home', to: 'static_pages#home'
@@ -6,6 +8,9 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get  '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'static_pages#home'
