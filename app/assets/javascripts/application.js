8c7aaa8cbfa002a3992aa76ce92c2bc499bfb3fb
ajohe <arthur.johe@gmail.com>
2018-05-19 22:25:59 -0400

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require bootstrap
-//= require rails-ujs
-//= require turbolinks
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require rails-ujs
+//= require turbolinks
 //= require chartkick
 //= require_tree .
